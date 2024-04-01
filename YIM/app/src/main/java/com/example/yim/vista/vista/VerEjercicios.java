05e43b9a4bd652746e2a002b367bb104bfd5184d
SergioGomezCuquejo <sergiogomezcuquejo@gmail.com>
2024-04-01 18:11:31 +0200

    Agrego el popup que permite ver la información de los ejercicios.

--- YIM/app/src/main/java/com/example/yim/vista/vista/VerEjercicios.java
+++ YIM/app/src/main/java/com/example/yim/vista/vista/VerEjercicios.java
@@ -4,24 +4,30 @@ import static com.example.yim.vista.controlador.CambiarActivity.cambiar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.yim.R;
 
 public class VerEjercicios extends AppCompatActivity implements View.OnClickListener {
+    RelativeLayout ejercicio1;
     ImageView agregar_ejercicio;
     FrameLayout imagen_casa, imagen_calendario, imagen_estadisticas, imagen_usuario;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_ejercicios);
 
         //Referencias de las vistas
+        ejercicio1 = findViewById(R.id.ejercicio1);
+
         agregar_ejercicio = findViewById(R.id.agregar_ejercicio);
 
         imagen_casa = findViewById(R.id.imagen_casa);
@@ -30,6 +36,8 @@ public class VerEjercicios extends AppCompatActivity implements View.OnClickList
         imagen_usuario = findViewById(R.id.imagen_usuario);
 
         //Listeners
+        ejercicio1.setOnClickListener(this);
+
         agregar_ejercicio.setOnClickListener(this);
 
         imagen_casa.setOnClickListener(this);
@@ -41,7 +49,10 @@ public class VerEjercicios extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        if (id == R.id.agregar_ejercicio) {
+        if (id == R.id.ejercicio1) {
+            cambiarActivity(PopupVerEjercicios.class);
+
+        } else if (id == R.id.agregar_ejercicio) {
             cambiarActivity(PopupCrearEjercicios.class);
 
         } else if (id == R.id.imagen_casa){
