18fa6396fd68aa7f84834d7b6a1da3fb6b9ea7e1
SergioGomezCuquejo <sergiogomezcuquejo@gmail.com>
2024-04-07 13:46:55 +0200

    Agrego los avances de la interfaz de Estadísticas.

--- YIM/app/src/main/java/com/example/yim/vista/vista/Estadisticas.java
+++ YIM/app/src/main/java/com/example/yim/vista/vista/Estadisticas.java
@@ -1,16 +1,59 @@
 package com.example.yim.vista.vista;
 
+import static com.example.yim.vista.controlador.CambiarActivity.cambiar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.example.yim.R;
 
-public class Estadisticas extends AppCompatActivity {
+public class Estadisticas extends AppCompatActivity implements View.OnClickListener {
 
+    FrameLayout imagen_casa, imagen_calendario, imagen_estadisticas, imagen_usuario;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas);
+
+        //Referencias de las vistas
+        imagen_casa = findViewById(R.id.imagen_casa);
+        imagen_calendario = findViewById(R.id.imagen_calendario);
+        imagen_estadisticas = findViewById(R.id.imagen_estadisticas);
+        imagen_usuario = findViewById(R.id.imagen_usuario);
+
+        //Listeners
+        imagen_casa.setOnClickListener(this);
+        imagen_calendario.setOnClickListener(this);
+        imagen_estadisticas.setOnClickListener(this);
+        imagen_usuario.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.imagen_casa){
+            cambiarActivity(Inicio.class);
+
+        } else if (id == R.id.imagen_calendario) {
+            cambiarActivity(RutinaSemanal.class);
+
+        } else if (id == R.id.imagen_estadisticas) {
+            cambiarActivity(this.getClass());
+
+        } else if (id == R.id.imagen_usuario) {
+            cambiarActivity(Perfil.class);
+
+        }
+    }
+
+    private void cambiarActivity(Class<?> activity) {
+        cambiar(this, activity);
     }
 }
